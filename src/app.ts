6e2679a0ab8d55e4fa140c29f9467f04101402e0
Mariana Fernanda <ariana.fernanda@icloud.com>
2022-12-04 23:04:25 -0300

    Reqs 1 e 2

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,13 @@
 import express from 'express';
+import ProductController from './controllers/products.controller';
 
 const app = express();
 
 app.use(express.json());
 
+const productsController = new ProductController();
+
+app.post('/products', productsController.create);
+app.get('/products', productsController.getAll);
+
 export default app;
-//
---
+++ src/models/products.model.ts
@@ -0,0 +1,27 @@
+import { Pool, ResultSetHeader } from 'mysql2/promise';
+import Product from '../../interfaces/product.interface';
+
+export default class ProductModel {
+  public connection: Pool;
+
+  constructor(connection: Pool) {
+    this.connection = connection;
+  }
+
+  public async create(product: Product): Promise<Product> {
+    const { name, amount } = product;
+    const result = await this.connection.execute<ResultSetHeader>(
+      'INSERT INTO Trybesmith.Products (name, amount) VALUES (?, ?)',
+      [name, amount],
+    );
+    const [dataInserted] = result;
+    const { insertId } = dataInserted;
+    return { id: insertId, ...product };
+  }
+
+  public async getAll(): Promise<Product[]> {
+    const result = await this.connection.execute('SELECT * FROM Trybesmith.Products;');
+    const [row] = result;
+    return row as Product[];
+  }
+}
